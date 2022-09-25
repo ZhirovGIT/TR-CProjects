ffa9a7b8f5c8e659e1c86a8d0cad8c4b3bc063e3
Dmitrii <111457490+ZhirovGIT@users.noreply.github.com>
2022-09-26 01:38:48 +0300

    Load second homework

---
+++ CProjects/HW_2/myFirstAppl.c
@@ -0,0 +1,15 @@
+// myFirstAppl.c -- shows given text in console
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("I am a programmer.\n\n");	// print text on 1 line
+
+	printf("Hello, \n"
+			"i \n"
+			"am \n"
+			"Dmitrii.");				// prints text on 4 lines
+
+	return 0;
+}
