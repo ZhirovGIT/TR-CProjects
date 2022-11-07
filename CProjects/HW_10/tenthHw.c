c6ba7de48d1592a84ea77fadc70a61be33b72970
Dmitrii <111457490+ZhirovGIT@users.noreply.github.com>
2022-11-07 19:02:37 +0200

    Update tenthHw.c

--- CProjects/HW_10/tenthHw.c
+++ CProjects/HW_10/tenthHw.c
@@ -7,7 +7,7 @@ int xPower (int x, int power);		// for task 3
 
 int main()
 {
-	int number, x, power;
+	int number, x, power, res01, res02, res03;
 
 	// Task 1.
 	printf("Task 1.\n");
@@ -16,7 +16,8 @@ int main()
 	fflush(stdout);
 	scanf("%d", &number);
 
-	evenDigitsCount(number);
+	res01 = evenDigitsCount(number);
+	printf("Count of even digits: %d\n\n", res01);
 
 	// Task 2.
 	printf("Task 2.\n");
@@ -25,7 +26,8 @@ int main()
 	fflush(stdout);
 	scanf("%d", &number);
 
-	sumDigits(number);
+	res02 = sumDigits(number);
+	printf("Sum of digits: %d\n\n", res02);
 
 	// Task 3.
 	printf("Task 3.\n");
@@ -38,7 +40,8 @@ int main()
 	fflush(stdout);
 	scanf("%d", &power);
 
-	xPower(x, power);
+	res03 = xPower(x, power);
+	printf("Result: %d", res03);
 
 	return 0;
 }
@@ -53,13 +56,13 @@ int evenDigitsCount (int number)
 		printf("Wrong number!\n");
 		return -1;
 	}
+	// if (number < 0) number = -number;
 
-	while (number != 0)
+	do
 	{
 		if (number % 2 == 0) count++;
 		number /= 10;
-	}
-	printf("Count of even digits: %d\n\n", count);
+	} while (number != 0);
 
 	return count;
 }
@@ -69,13 +72,11 @@ int sumDigits (int number)
 {
 	int sum = 0;
 
-	while (number > 0)
+	do
 	{
 		sum += number % 10;
 		number /= 10;
-	}
-	printf("Sum of digits: %d\n\n", sum);
-
+	} while (number != 0);
 
 	return sum;
 }
@@ -85,44 +86,35 @@ int xPower (int x, int power)
 {
 	// Option 1
 	/*
-	int count = 2;
+		int count = 2;
+		if (power == 0) return 1;
+		else if (power == 1) return x;
+		else if (x == 0 && power == 0)
+		{
+			printf("ERROR!\n");
+			return -1;
+		}
+		do
+		{
+			x *= x;
+			count++;
+		} while (count != power);
+	*/
 
-	if (power == 0) return 1;
-	else if (power == 1) return x;
-	else if (x == 0 && power == 0)
+	// Option 2
+	int result = 1;
+
+	if (power == 0)
 	{
-		printf("ERROR!\n");
-		return -1;
+		printf("Result: %d", x = 1);
+		return 1;
 	}
 
 	do
 	{
-		x *= x;
-		count++;
-	} while (count != power);
-	printf("%d", x);
-	*/
+		result *= x;
+		power--;
+	} while (power != 0);
 
-	// Option 2
-	int xPower (int x, int power)
-	{
-		int result = 1;
-
-		if (power == 0)
-		{
-			printf("Result: %d", x = 1);
-			return 1;
-		}
-
-		do
-		{
-			result *= x;
-			power--;
-		} while (power != 0);
-		printf("Result: %d", result);
-
-		return result;
-	}
-
-	return x;
+	return result;
 }
